a9aa93a175d416e523bc75b24e8e727ede2df3cd
fa1ez <91018317+fa1ez@users.noreply.github.com>
2021-12-02 23:49:40 +0500

    Add files via upload

---
+++ A4/src/application/SampleController.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+
+public class SampleController {
+
+    @FXML
+    private ToggleGroup choice;
+
+    @FXML
+    private Button B1;
+
+    @FXML
+    void func1(ActionEvent event) {
+    	
+    }
+
+}
